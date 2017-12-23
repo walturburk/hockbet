bc90975606fb66cf5a7209329b9640af7eadf5fd
Walter Hietanen <hietanen@gmail.com>
2017-12-24 00:37:26 +0100

    Mysql select test

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,29 @@
 var express = require('express');
 var router = express.Router();
+var mysql = require('mysql');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
+
+  var mysql      = require('mysql');
+ var connection = mysql.createConnection({
+   host     : 'localhost',
+   user     : 'hockeyapp',
+   password : 'actioncop',
+   database : 'hockey'
+ });
+
+ connection.connect();
+
+ connection.query('SELECT * FROM hockey.users', function(err, rows, fields) {
+   if (!err)
+     console.log('The solution is: ', rows);
+   else
+     console.log('Error while performing Query.');
+ });
+
+ connection.end();
+
   res.render('index', { title: 'Express' });
 });
 
