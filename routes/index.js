13406724a23f0572af67f6e18ebbb2bd8daf8c1b
Walter Hietanen <hietanen@gmail.com>
2017-12-31 14:59:26 +0100

    config file used for db connect

--- routes/index.js
+++ routes/index.js
@@ -1,16 +1,17 @@
 var express = require('express');
 var router = express.Router();
 var mysql = require('mysql');
+var config = require('./config.json');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
 
   var mysql      = require('mysql');
  var connection = mysql.createConnection({
-   host     : 'localhost',
-   user     : 'hockeyapp',
-   password : 'actioncop',
-   database : 'hockey'
+   host     : config.host,
+   user     : config.user,
+   password : config.password,
+   database : config.database
  });
 
  connection.connect();
