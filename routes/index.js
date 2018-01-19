5bd64d02387c26a4bd6b9319a23bc8ef511a5f5d
Walter Hietanen <hietanen@gmail.com>
2018-01-19 16:23:07 +0100

    standings.js

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var router = express.Router();
 var mysql = require('mysql');
-var config = require('./config.json');
+var config = require('./../config.json');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
---
+++ routes/standings.js
@@ -0,0 +1,24 @@
+var express = require('express');
+var router = express.Router();
+const https = require('https');
+
+router.get('/', function(req, res, next) {
+
+  https.get('https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY', (resp) => {
+    let data = '';
+
+    // A chunk of data has been recieved.
+    resp.on('data', (chunk) => {
+      data += chunk;
+    });
+
+    // The whole response has been received. Print out the result.
+    resp.on('end', () => {
+      console.log(JSON.parse(data).explanation);
+    });
+
+  }).on("error", (err) => {
+    console.log("Error: " + err.message);
+  });
+
+}
