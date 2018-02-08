52d173ab47966c1d1854b20df2f9768813cd8bdd
Walter Hietanen <hietanen@gmail.com>
2018-02-08 17:46:18 +0100

    createConnection instead of pool for now

--- database.js
+++ database.js
@@ -4,16 +4,14 @@ var connection;
 
 function connectToDatabase() {
   if (!connection) {
-    var connection = mysql.createPool({
+    var connection = mysql.createConnection({
       host     : config.host,
       user     : config.user,
       password : config.password,
-      database : config.database,
-      connectionLimit: 10,
-      supportBigNumbers: true
+      database : config.database
     });
 
-    connection.connect();
+    connection.getConnection();
   }
   return connection;
 }
