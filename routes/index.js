b4f6cb12d797d9d8c19b3f1e4816d0c9037f2c7b
Walter Hietanen <hietanen@gmail.com>
2018-03-17 23:50:44 +0100

    with shit in index.js

--- routes/index.js
+++ routes/index.js
@@ -25,6 +25,30 @@ router.post('/logout', function(req, res, next) {
   res.redirect('back');
 });
 
+router.get('/dbsave', function(req, res, next) {
+  mysqldumper.save();
+  res.send("Current database saved to dump file");
+});
 
+router.get('/dbload', function(req, res, next) {
+  mysqldumper.load();
+  res.send("Database loaded from dump file");
+});
+
+router.get('/creategroup', function(req, res, next) {
+  var groupname = req.query.name;
+  betz.createNewGroup(groupname, 1718);
+  res.send('ok its done: '+groupname);
+});
+
+router.get('/populategroup', function(req, res, next) {
+  betz.populateGroup(1);
+  res.send('ok its done');
+});
+
+router.get('/test', function(req, res, next) {
+  betz.getShitFromApi();
+  res.send("k");
+});
 
 module.exports = router;
